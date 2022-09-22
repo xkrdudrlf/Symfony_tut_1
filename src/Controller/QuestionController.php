116750fd45ce3460d190a98f0742beac8b8e35fc
tom <tomtak1014@gmail.com>
2022-09-23 09:50:11 +1000

    d

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+/*
+    AbstractController class gives 
+*/
+class QuestionController extends AbstractController
 {
     #[Route('/', name: 'blog_list')]
     public function homepage()
@@ -16,8 +20,18 @@ class QuestionController
     #[Route('/questions/{slug}')]
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show the question "%s" !', ucwords(str_replace('-', ' ', $slug))
-        ));
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe.. try saying the spell backwards?',
+        ];
+
+        /*
+            render(string: 'path of the template', array: list of variables we want to pass to the template)
+        */
+        return $this->render('question/show.html.twig',[
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+        ]);
     }
 } 
